2d90d6243fd7c0433e3fc6c8aa4ea4f23448292c
John O'Neil <fluffywestie@tpg.com.au>
2020-06-15 22:30:25 +1000

    Updated GitIgnore and server.js

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
+// BoilerPlate Express Server
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 app.use(express.static("public"));
